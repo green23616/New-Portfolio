46b676cc9c869ff6deb583a3b9c6eac075ae5721
jwk <green23616@gmail.com>
2024-12-24 10:13:35 +0900

    Vercel 배포 테스트 중 버그 수정 - 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router';
 // Components
 import Main from './pages/main/Main';
-import Story from './pages/story/Story';
 import Photos from './pages/photos/Photos';
-import Skills from './pages/skills/Skills';
-import Works from './pages/works/Works';
-import Music from './pages/music/Music';
-import Study from './pages/study/Study';
 
 function App() {
   return (
@@ -14,11 +9,6 @@ function App() {
       <Routes>
         <Route index element={<Main />} />
         <Route path="/photos" element={<Photos />} />
-        <Route path="/story" element={<Story />} />
-        <Route path="/skills" element={<Skills />} />
-        <Route path="/works" element={<Works />} />
-        <Route path="/music" element={<Music />} />
-        <Route path="/study" element={<Study />} />
       </Routes>
     </BrowserRouter>
   );
